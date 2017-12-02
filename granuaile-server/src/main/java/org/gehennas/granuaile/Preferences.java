fceabcaf9536b75e2d6726c7c06d873aa0a2f838
Nikita Zverev <hamsternsk@gmail.com>
2017-12-02 23:56:19 +0700

    Preferences improvements

--- granuaile-server/src/main/java/org/gehennas/granuaile/Preferences.java
+++ granuaile-server/src/main/java/org/gehennas/granuaile/Preferences.java
@@ -2,25 +2,24 @@ package org.gehennas.granuaile;
 
 import org.apache.commons.cli.*;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Preferences {
 
     private static final String PORT = "port";
     private static final String USER_DATA = "userData";
 
-    private int port = 8080;
-    private String userDataLocation = System.getProperty("user.home") + "\\.granuaile";
+    private final int port;
+    private final Path userDataLocation;
 
     public Preferences(String... args) throws PrepareException{
-        CommandLine cmdOptions = createOptions(args);
-        if (cmdOptions.hasOption(PORT)) {
-            try {
-                port = Integer.parseInt(cmdOptions.getOptionValue(PORT));
-            } catch (NumberFormatException e) {
-                throw new PrepareException(e);
-            }
-        }
-        if (cmdOptions.hasOption(USER_DATA)) {
-            userDataLocation = cmdOptions.getOptionValue(USER_DATA);
+        CommandLine cmd = createOptions(args);
+        try {
+            port = cmd.hasOption(PORT) ? (Integer) cmd.getParsedOptionValue(PORT) : 8080;
+            userDataLocation = cmd.hasOption(USER_DATA) ? Paths.get(cmd.getOptionValue(USER_DATA)) : Paths.get(System.getProperty("user.home"), ".granuaile");
+        } catch (ParseException| NumberFormatException e) {
+            throw new PrepareException(e);
         }
     }
 
@@ -28,14 +27,16 @@ public class Preferences {
         return port;
     }
 
+    public Path getUserDataLocation() {
+        return userDataLocation;
+    }
+
     private CommandLine createOptions(String... args) throws PrepareException{
         final Options options = new Options();
         options.addOption(Option.builder().longOpt(USER_DATA).hasArg(true).desc("Application data location").build());
-        options.addOption(Option.builder().longOpt(PORT).hasArg(true).desc("Server Port").build());
+        options.addOption(Option.builder().longOpt(PORT).hasArg(true).desc("Server Port").type(Integer.class).build());
 
         CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-
         try {
             return parser.parse(options, args);
         } catch (ParseException e) {
